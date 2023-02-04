2af7e7152df2257c15615adfdbe58dc8c558f682
Matt-Kou <36983956+Matt-Kou@users.noreply.github.com>
2023-02-03 19:39:42 -0500

    controller input finished

--- js/scenes/scenes.js
+++ js/scenes/scenes.js
@@ -1,17 +1,25 @@
 import * as global from "../global.js";
-import { Gltf2Node } from "../render/nodes/gltf2.js";
+import {Gltf2Node} from "../render/nodes/gltf2.js";
 
 export default () => {
-   global.scene().addNode(new Gltf2Node({
-      url: ""
-   })).name = "backGround";
+    global.scene().addNode(new Gltf2Node({
+        url: ""
+    })).name = "backGround";
 
-   return {
-      enableSceneReloading: true,
-      scenes: [
-         { name: "DemoExample" , path: "./demoExample.js" },
-         // { name: "robot" , path: "./robot.js" },
-         { name: "BallBounce" , path: "./ball_bounce.js" },
-      ]
-   };
+    return {
+        enableSceneReloading: true,
+        scenes: [
+            {name: "DemoExample", path: "./demoExample.js"},
+            // { name: "robot" , path: "./robot.js" },
+            {name: "BallBounce", path: "./ball_bounce.js"},
+            {name: "BallBounce2", path: "./ballBounce2.js"},
+            {name: "BallBounce3", path: "./ballBounce3.js"},
+            {name: "buttonTest", path: "./buttonTest.js"},
+            // {name: "buttonTest2", path: "./buttonTest2.js"},
+            {name: "buttonAndCollision", path: "./buttonTest3.js"},
+            {name: "ctrlTest", path: "./ctrlTest.js"},
+            {name: "ctrlTest2", path: "./ct2.js"},
+            {name: "ctrlTest3", path: "./ctrlTest3.js"},
+        ]
+    };
 }
