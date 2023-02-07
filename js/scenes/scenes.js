189c9b7e2037aedfe179a9b1b057551f845650f1
Matt-Kou <36983956+Matt-Kou@users.noreply.github.com>
2023-02-07 03:13:45 -0500

    tt01 finished

--- js/scenes/scenes.js
+++ js/scenes/scenes.js
@@ -10,18 +10,20 @@ export default () => {
         enableSceneReloading: true,
         scenes: [
             {name: "DemoExample", path: "./demoExample.js"},
-            {name: "robot", path: "./robot.js"},
-            {name: "BallBounce", path: "./ball_bounce.js"},
-            {name: "BallBounce2", path: "./ballBounce2.js"},
-            {name: "BallBounce3", path: "./ballBounce3.js"},
-            {name: "buttonTest", path: "./buttonTest.js"},
-            // {name: "buttonTest2", path: "./buttonTest2.js"},
+            // {name: "robot", path: "./robot.js"},
+            // {name: "BallBounce", path: "./ball_bounce.js"},
+            // {name: "BallBounce2", path: "./ballBounce2.js"},
+            // {name: "BallBounce3", path: "./ballBounce3.js"},
+            // {name: "buttonTest", path: "./buttonTest.js"},
+            // // {name: "buttonTest2", path: "./buttonTest2.js"},
             {name: "buttonAndCollision", path: "./buttonTest3.js"},
             {name: "ctrlTest", path: "./ctrlTest.js"},
             {name: "ctrlTest2", path: "./ct2.js"},
             {name: "ctrlTest3", path: "./ctrlTest3.js"},
             {name: "hit", path: "./hit.js"},
             {name: "grab", path: "./grab.js"},
+            {name: "tt01", path: "./tt.js"},
+            {name: "proj", path: "./projPlane.js"},
         ]
     };
 }
